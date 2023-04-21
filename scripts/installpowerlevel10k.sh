2e4be465a450a6e66020495ca0f7faa0a9bc31ab
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-04-21 21:12:06 +0200

    udpate

--- scripts/installpowerlevel10k.sh
+++ scripts/installpowerlevel10k.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install git zsh -y 
+# instal git and zsh
+sudo apt update && sudo apt install git zsh -y
 
+# install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# 
+
+# change deafult shell to zsh
 sudo chsh -s $(which zsh)
