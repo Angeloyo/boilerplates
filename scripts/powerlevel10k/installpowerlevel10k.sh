2a322351bea60a90aef19813888d91fd71a7193c
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-04-22 00:21:47 +0200

    update

--- scripts/powerlevel10k/installpowerlevel10k.sh
+++ scripts/powerlevel10k/installpowerlevel10k.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Go to home directory
+cd ~
 # instal git and zsh
 sudo apt update && sudo apt install git zsh curl -y
 echo "----------separation line---------"
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# when installation finishes you will enter zsh, type exit
 echo "----------separation line---------"
 # install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo "----------separation line---------"
 # change deafult shell to zsh
 chsh -s $(which zsh)
+# update theme of ohmyzsh
+echo "\n#Update theme" >> .zshrc
+echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> .zshrc
