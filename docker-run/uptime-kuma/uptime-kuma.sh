a4cf3f28a4de9b7ba01f0a3a215a77c99cdc8416
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-05-11 13:59:24 +0200

    added uptime-kuma

---
+++ docker-run/uptime-kuma/uptime-kuma.sh
@@ -0,0 +1,4 @@
+# https://uptime.kuma.pet/
+# https://github.com/louislam/uptime-kuma
+
+sudo docker run -d --restart=always -p 3001:3001 -v uptime-kuma:/app/data --name uptime-kuma louislam/uptime-kuma:1
