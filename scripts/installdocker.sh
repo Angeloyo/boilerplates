3ea10874fc0ba8d6a3aedd9f27c4c84de858d1ce
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2024-03-06 21:30:06 +0100

    Update installdocker.sh

--- scripts/installdocker.sh
+++ scripts/installdocker.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# wget https://raw.githubusercontent.com/Angeloyo/boilerplates/main/scripts/installdocker.sh -O /tmp/install.sh; bash /tmp/install.sh
+
 #install docker ubuntu 
 
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
 sudo apt-get update
 
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg -y
 
 sudo install -m 0755 -d /etc/apt/keyrings
 
@@ -18,12 +20,12 @@ echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docke
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 sudo docker run hello-world
 
 # optional install portainer
 
-sudo docker volume create portainer_data
+# sudo docker volume create portainer_data
 
-sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+# sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
